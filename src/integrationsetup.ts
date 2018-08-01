bd5842bea284e5de599aeb4344c4d71f5521ad39
James Waples <jamwaffles@gmail.com>
2018-08-01 15:47:40 +0100

    Fix dumb TSC error
    Everything has to be in src/ apparently

--- src/integrationsetup.ts
+++ src/integrationsetup.ts
@@ -4,11 +4,11 @@ import { Future, Option, None, Some } from 'funfix';
 import { stub } from 'sinon';
 import * as fs from 'fs';
 
-import { getDbConnection } from './src/test-helpers';
+import { getDbConnection } from './test-helpers';
 
 test('Integration test setup', async (t) => {
   // Create DB structure
-  await getDbConnection().query(fs.readFileSync("./db.sql", { encoding: 'utf8' }));
+  await getDbConnection().query(fs.readFileSync(__dirname + "/../db.sql", { encoding: 'utf8' }));
 
   // Insert sample data
   // await client.query(fs.readFileSync("./integration.sql", { encoding: 'utf8' }));
