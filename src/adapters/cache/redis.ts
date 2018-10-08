4903a51620ae647727c1e683853b1705d089e695
Peter East <petereast@pm.me>
2018-10-08 16:39:42 +0100

    Clean up and make initialisation external

--- src/adapters/cache/redis.ts
+++ src/adapters/cache/redis.ts
@@ -2,29 +2,19 @@ import { Option, None } from 'funfix';
 import { CacheAdapter, Event, EventData, EventContext, CacheEntry, Logger } from '../../.';
 import * as redis from 'redis';
 
-export function createRedisCacheAdapter(logger: Logger = console): CacheAdapter {
-  // init code here
-
-  const redis_client = redis.createClient();
-  redis_client.on("error", (err) => {
-    logger.error("REDIS ERROR");
-  });
-
+export function createRedisCacheAdapter(redis_client: any, logger: Logger = console): CacheAdapter {
   async function get<T extends CacheEntry<any>>(id: string): Promise<Option<T>> {
     return Option.of(
-      redis_client.get(id) as any,
+      JSON.parse(redis_client.get(id) as any),
     );
   }
 
   function set(id: string, entry: CacheEntry<any>): Promise<void> {
-    redis_client.sadd("cache_content", id, (err, operation_return) => {
+    redis_client.sadd("cache_content", id, (err: any, operation_return: any) => {
       if (operation_return === 1) {
         redis_client.set(id, JSON.stringify(entry));
-      } else {
-        // Do nothing
       }
     });
-
     return Promise.resolve();
   }
 
