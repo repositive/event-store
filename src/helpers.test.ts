71c35a676e1503b0733119d525af1b5b154232b6
James Waples <jamwaffles@gmail.com>
2018-10-24 11:14:58 +0100

    Lint and format

--- src/helpers.test.ts
+++ src/helpers.test.ts
@@ -2,92 +2,92 @@ import { test } from 'ava';
 import { id } from './test-helpers';
 import { createEvent, createContext, EventData, EventContext, Event } from '.';
 
-test('creates an event with default fields filled', t => {
-	const evt = createEvent(
-		{ event_type: 'Type', event_namespace: 'ns', data: { foo: 'bar' } as any },
-		() => id,
-		() => '2018-01-02 03-04-05'
-	);
+test('creates an event with default fields filled', (t) => {
+  const evt = createEvent(
+    { event_type: 'Type', event_namespace: 'ns', data: { foo: 'bar' } as any },
+    () => id,
+    () => '2018-01-02 03-04-05',
+  );
 
-	const expected = {
-		id,
-		data: {
-			type: 'ns.Type',
-			event_type: 'Type',
-			event_namespace: 'ns',
-			foo: 'bar'
-		},
-		context: {
-			subject: {},
-			time: '2018-01-02 03-04-05'
-		}
-	};
+  const expected = {
+    id,
+    data: {
+      type: 'ns.Type',
+      event_type: 'Type',
+      event_namespace: 'ns',
+      foo: 'bar',
+    },
+    context: {
+      subject: {},
+      time: '2018-01-02 03-04-05',
+    },
+  };
 
-	t.deepEqual(evt, expected);
+  t.deepEqual(evt, expected);
 });
 
-test('creates an event with a given context', t => {
-	const evt = createEvent(
-		{
-			event_type: 'Type',
-			event_namespace: 'ns',
-			data: { foo: 'bar' } as any,
-			context: { subject: { bar: 'baz' }, time: new Date().toISOString() }
-		},
-		() => id,
-		() => '2018-01-02 03-04-05'
-	);
+test('creates an event with a given context', (t) => {
+  const evt = createEvent(
+    {
+      event_type: 'Type',
+      event_namespace: 'ns',
+      data: { foo: 'bar' } as any,
+      context: { subject: { bar: 'baz' }, time: new Date().toISOString() },
+    },
+    () => id,
+    () => '2018-01-02 03-04-05',
+  );
 
-	const expected = {
-		id,
-		data: {
-			type: 'ns.Type',
-			event_type: 'Type',
-			event_namespace: 'ns',
-			foo: 'bar'
-		},
-		context: {
-			subject: { bar: 'baz' },
-			time: '2018-01-02 03-04-05'
-		}
-	};
+  const expected = {
+    id,
+    data: {
+      type: 'ns.Type',
+      event_type: 'Type',
+      event_namespace: 'ns',
+      foo: 'bar',
+    },
+    context: {
+      subject: { bar: 'baz' },
+      time: '2018-01-02 03-04-05',
+    },
+  };
 
-	t.deepEqual(evt, expected);
+  t.deepEqual(evt, expected);
 });
 
-test('creates a context with subject and no action', t => {
-	const evt = createEvent(
-		{
-			event_type: 'Type',
-			event_namespace: 'ns',
-			data: { foo: 'bar' } as any,
-			context: createContext({ bar: 'baz' })
-		},
-		() => id,
-		() => '2018-01-02 03-04-05'
-	);
+test('creates a context with subject and no action', (t) => {
+  const evt = createEvent(
+    {
+      event_type: 'Type',
+      event_namespace: 'ns',
+      data: { foo: 'bar' } as any,
+      context: createContext({ bar: 'baz' }),
+    },
+    () => id,
+    () => '2018-01-02 03-04-05',
+  );
 
-	t.is(evt.context.action, undefined);
-	t.deepEqual(evt.context.subject, { bar: 'baz' });
+  t.is(evt.context.action, undefined);
+  t.deepEqual(evt.context.subject, { bar: 'baz' });
 });
 
-test('creates a context with subject and an action', t => {
-	const evt = createEvent(
-		{
-			event_type: 'Type',
-			event_namespace: 'ns',
-			data: { foo: 'bar' } as any,
-			context: createContext({ bar: 'baz' }, 'someRandomAction', () => '2018-01-02 03-04-05')
-		},
-		() => id,
-		() => '2018-01-02 03-04-05'
-	);
+test('creates a context with subject and an action', (t) => {
+  const evt = createEvent(
+    {
+      event_type: 'Type',
+      event_namespace: 'ns',
+      data: { foo: 'bar' } as any,
+      context: createContext({ bar: 'baz' }, 'someRandomAction', () => '2018-01-02 03-04-05'),
+    },
+    () => id,
+    () => '2018-01-02 03-04-05',
+  );
 
-	const expected = {
-		action: 'someRandomAction',
-		subject: { bar: 'baz' },
-		time: '2018-01-02 03-04-05'
-	};
+  const expected = {
+    action: 'someRandomAction',
+    subject: { bar: 'baz' },
+    time: '2018-01-02 03-04-05',
+  };
 
-	t.deepEqual(evt.context, expected);
+  t.deepEqual(evt.context, expected);
 });
--- src/helpers.ts
+++ src/helpers.ts
@@ -2,55 +2,55 @@ import { EventData, EventContext, Event } from '.';
 import { v4 } from 'uuid';
 
 function defaultContext(): EventContext<{}> {
-	return {
-		subject: {},
-		time: new Date().toISOString()
-	};
+  return {
+    subject: {},
+    time: new Date().toISOString(),
+  };
 }
 
 export function createEvent<D extends EventData>(
-	{
-		event_type,
-		event_namespace,
-		data,
-		context = defaultContext()
-	}: {
-		event_type: string;
-		event_namespace: string;
-		data: D;
-		context?: EventContext<any>;
-	},
-	_uuid: () => string = v4,
-	_time: () => string = () => new Date().toISOString()
+  {
+    event_type,
+    event_namespace,
+    data,
+    context = defaultContext(),
+  }: {
+    event_type: string;
+    event_namespace: string;
+    data: D;
+    context?: EventContext<any>;
+  },
+  _uuid: () => string = v4,
+  _time: () => string = () => new Date().toISOString(),
 ): Event<EventData, EventContext<any>> {
-	const d = {
-		...(data as object),
-		type: `${event_namespace}.${event_type}`,
-		event_type,
-		event_namespace
-	};
+  const d = {
+    ...(data as object),
+    type: `${event_namespace}.${event_type}`,
+    event_type,
+    event_namespace,
+  };
 
-	// Allow overriding of time value for testing purposes
-	const c: EventContext<any> = {
-		...context,
-		time: _time()
-	};
+  // Allow overriding of time value for testing purposes
+  const c: EventContext<any> = {
+    ...context,
+    time: _time(),
+  };
 
-	return {
-		data: d,
-		context: c,
-		id: _uuid()
-	};
+  return {
+    data: d,
+    context: c,
+    id: _uuid(),
+  };
 }
 
 export function createContext(
-	subject: object,
-	action?: string,
-	_time: () => string = () => new Date().toISOString()
+  subject: object,
+  action?: string,
+  _time: () => string = () => new Date().toISOString(),
 ): EventContext<any> {
-	return {
-		action,
-		subject,
-		time: _time()
-	};
+  return {
+    action,
+    subject,
+    time: _time(),
+  };
 }
