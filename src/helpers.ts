afc2a741300a0af16ccc5709799405f70b044b96
James Waples <jamwaffles@gmail.com>
2018-10-24 11:49:29 +0100

    [F] TA-116 Add helper to create complete events (#21)
    * [F] TA-116 Event creation helper functions
    
    * Lint and format
    
    * Change API of createEvent to be more ergonomic
    
    * Allow any object as data
    
    * Remove anys

---
+++ src/helpers.ts
@@ -0,0 +1,42 @@
+import { EventData, EventContext, Event } from '.';
+import { v4 } from 'uuid';
+
+function defaultContext(): EventContext<{}> {
+  return {
+    subject: {},
+    time: new Date().toISOString(),
+  };
+}
+
+export function createEvent(
+  event_namespace: string,
+  event_type: string,
+  data: object,
+  context: EventContext<any> = defaultContext(),
+  _uuid: () => string = v4,
+): Event<EventData, EventContext<any>> {
+  const d = {
+    ...data,
+    type: `${event_namespace}.${event_type}`,
+    event_type,
+    event_namespace,
+  };
+
+  return {
+    data: d,
+    context,
+    id: _uuid(),
+  };
+}
+
+export function createContext(
+  subject: object,
+  action?: string,
+  _time: () => string = () => new Date().toISOString(),
+): EventContext<any> {
+  return {
+    action,
+    subject,
+    time: _time(),
+  };
+}
