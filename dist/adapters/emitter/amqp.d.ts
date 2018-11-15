16c5e28b4b0acece60cf2807b83c1cb86f8c02e1
James Waples <jamwaffles@gmail.com>
2018-11-15 16:35:06 +0000

    Fix Iris namespace opts

--- dist/adapters/emitter/amqp.d.ts
+++ dist/adapters/emitter/amqp.d.ts
@@ -1,3 +1,7 @@
 import { EmitterAdapter, Logger } from '../../.';
+export interface IrisOptions {
+    uri?: string;
+    namespace: string;
+}
 export declare function wait(n: number): Promise<void>;
-export declare function createAQMPEmitterAdapter(connectionString: string, logger?: Logger): EmitterAdapter;
+export declare function createAQMPEmitterAdapter(irisOpts: IrisOptions, logger?: Logger): EmitterAdapter;
