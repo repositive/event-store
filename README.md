5fb802091a55f2db6ff86d5420414a7b1c2e90a7
James Waples <james@wapl.es>
2019-01-14 11:12:51 +0000

    [S] Basic release process (#44)
    * Add some more metadata
    
    * WIP Add post-publish script
    
    * Add publish instructions
    
    * Add publish metadata
    
    * build and publish docs in Git worktree
    
    * Bump
    
    * Add master update step

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ An implementation of the event sourcing paradigm in Typescript.
 
 ## [Documentation](https://repositive.github.io/event-store/doc)
 
-Start by looking at the [`EventStore`](https://repositive.github.io/event-store/doc/classes/eventstore.html) class.
+Start by looking at the `EventStore` class.
 
 ## Development
 
@@ -28,3 +28,19 @@ The database is started at `postgres://repositive:repositive@localhost:5433/even
 
 * `yarn doc`
 * Open `doc/index.html` in your favourite browser
+
+## Deploy
+
+```bash
+# Make sure we're deploying up to date master
+git checkout master && git pull
+
+# (optional) login to NPM
+npm login
+
+# Increment NPM version number
+npm version major|minor|patch
+
+# Publish to NPM
+npm publish
+```
---
+++ post-publish.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Post-publish hook. This script should not be run manually.
+# Debugging can be performed with `PATH="$PATH:$(npm bin)" ./post-publish.sh`
+# Requires `jq`; `apt install -y jq`
+
+set -e
+
+VERSION=$(cat package.json | jq .version -r)
+DOC_OUTPUT="./doc/${VERSION}"
+DOC_INDEX="./doc/index.html"
+# Temporary folder to add and upload new docs from
+GH_PAGES_WORKTREE=".gh-pages-tmp"
+
+echo "Build docs for version ${VERSION}"
+
+git worktree remove --force "${GH_PAGES_WORKTREE}" || true
+git worktree add "${GH_PAGES_WORKTREE}" gh-pages
+
+cd "${GH_PAGES_WORKTREE}"
+
+# Create a redirect from /doc to the latest version under /doc/a.b.c
+echo "<meta http-equiv=\"refresh\" content=\"0; url=https://repositive.github.io/event-store/doc/${VERSION}\" />" > "${DOC_INDEX}"
+
+typedoc \
+    --out "$DOC_OUTPUT" \
+    --exclude **/*.test.ts \
+    --exclude */*.test.ts \
+    --excludePrivate \
+    --mode file \
+    --name "Event store ${VERSION}" \
+    ./src
+
+echo "Adding generated docs"
+git add -f "${DOC_INDEX}"
+git add -f "${DOC_OUTPUT}"
+echo "Comitting"
+git commit -m "Add documentation for version ${VERSION}" || true
+
+echo "Uploading docs for version ${VERSION} to gh-pages"
+git push origin gh-pages
+
+git worktree remove "${GH_PAGES_WORKTREE}"
+
+echo
+echo "Success"
