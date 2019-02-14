b81c9f393e69abee1837388c540871b96a1585a8
Ruben Paz <me@ruben.io>
2019-02-14 22:39:58 +0000

    [S] Strictier listen type (#49)
    * [S] Improve listen type
    
    * Update docs
    
    * Update src/event-store.ts
    
    Co-Authored-By: Istar-Eldritch <me@ruben.io>
    
    * Add type tests

--- src/event-store.ts
+++ src/event-store.ts
@@ -358,7 +358,7 @@ export class EventStore<Q> {
 
   let store = new EventStore(...);
 
-  store.listen<SomeEvent>(
+  store.listen(
       'some_namespace',
       'SomeEvent',
       async (event: Event<SomeEvent, any>, store: EventStore) => {
@@ -379,7 +379,7 @@ export class EventStore<Q> {
   public async listen<T extends EventData>(
     event_namespace: T["event_namespace"],
     event_type: T["event_type"],
-    handler: EventHandler<Q, any>,
+    handler: EventHandler<Q, Event<T, EventContext<any>>>,
   ): Promise<void> {
     const pattern = [event_namespace, event_type].join(".");
 
