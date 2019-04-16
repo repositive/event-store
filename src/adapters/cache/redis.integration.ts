b20b1650f825e948393af8728c2098f8f743a0ca
James Waples <james@wapl.es>
2019-04-16 11:29:11 +0100

    Add a couple more Redis tests

--- src/adapters/cache/redis.integration.ts
+++ src/adapters/cache/redis.integration.ts
@@ -1,4 +1,5 @@
 import test from "ava";
+import { None } from 'funfix';
 import { v4 } from 'uuid';
 import * as pino from 'pino';
 import {
@@ -17,7 +18,37 @@ test("Set and retrieve Redis cache item", async (t: any) => {
 
   await cache.set(cache_id, cache_item);
 
-  const result = await(cache.get(cache_id));
+  const result = (await cache.get(cache_id));
 
   t.deepEqual(result.get(), cache_item);
+});
+
+test("Find nonexistent item", async (t: any) => {
+  const cache_id = v4();
+
+  const cache = createRedisCacheAdapter(cache_endpoint, pino());
+
+  const result = (await cache.get(cache_id));
+
+  t.deepEqual(result, None);
+});
+
+
+test("Update cache item", async (t: any) => {
+  const cache_id = v4();
+  const cache_item: CacheEntry<number> = { time: new Date().toISOString(), data: 100 };
+
+  const cache = createRedisCacheAdapter(cache_endpoint, pino());
+
+  await cache.set(cache_id, cache_item);
+
+  let result = (await cache.get(cache_id)).get();
+
+  result.data= 200;
+
+  await cache.set(cache_id, result);
+
+  const updated = await cache.get(cache_id);
+
+  t.deepEqual(updated.get(), { ...cache_item, data: 200 });
 });
