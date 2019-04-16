0f0d52c3683bfb1dd27dbca99544cc3adf1d1d25
James Waples <james@wapl.es>
2019-04-16 11:25:42 +0100

    Implement Redis cache with HSET/HGET

--- src/adapters/cache/redis.ts
+++ src/adapters/cache/redis.ts
@@ -1,25 +1,58 @@
-import { Option, None } from 'funfix';
+import { Option, None, Some } from 'funfix';
 import { CacheAdapter, Event, EventData, EventContext, CacheEntry, Logger } from '../../.';
+import { ClientOpts } from 'redis';
 import * as redis from 'redis';
 
-export function createRedisCacheAdapter(redis_client: any, logger: Logger = console): CacheAdapter {
+export { ClientOpts as RedisClientOptions } from 'redis';
+
+export function createRedisCacheAdapter(
+  redis_url: string,
+  logger: Logger = console,
+  client_options?: ClientOpts
+): CacheAdapter {
+  const redis_client = redis.createClient(redis_url, client_options);
+
   async function get<T extends CacheEntry<any>>(id: string): Promise<Option<T>> {
-    return Option.of(
-      JSON.parse(redis_client.get(id) as any),
-    );
+    logger.trace({ id }, 'eventStoreRedisCacheQuery');
+
+    try {
+      const result: string = await new Promise((resolve, reject) =>
+        redis_client.hget('aggregate_cache', id, (err: Error, item: string) =>
+          err ? reject(err) : resolve(item)
+        )
+      );
+
+      const opt: Option<T> = Option.of(result).map((result: string): T => JSON.parse(result));
+
+      logger.trace(opt, 'eventStoreRedisCacheResponse');
+
+      return opt;
+    } catch (e) {
+      logger.error(e.stack, 'eventStoreRedisCacheGetError');
+
+      return None;
+    }
   }
 
-  function set(id: string, entry: CacheEntry<any>): Promise<void> {
-    redis_client.sadd("cache_content", id, (err: any, operation_return: any) => {
-      if (operation_return === 1) {
-        redis_client.set(id, JSON.stringify(entry));
-      }
-    });
-    return Promise.resolve();
+  async function set(id: string, entry: CacheEntry<any>): Promise<void> {
+    logger.trace({ id, entry }, 'eventStoreRedisCacheSet');
+
+    try {
+      await new Promise((resolve, reject) =>
+        redis_client.hset(
+          'aggregate_cache',
+          id,
+          JSON.stringify(entry),
+          (err: Error, operation_return: number) => (err ? reject(err) : resolve(operation_return))
+        )
+      );
+    } catch (e) {
+      logger.error(e.stack, 'eventStoreRedisCacheSetError');
+    }
   }
 
   return {
     get,
-    set,
+    set
   };
 }
