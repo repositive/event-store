016a44ab69d3422f2a6ca18d57e416bdc835e133
James Waples <james@wapl.es>
2019-05-16 13:54:02 +0100

    Split types into own file

--- src/browser.ts
+++ src/browser.ts
@@ -1,24 +1,12 @@
 // Browser-only exports. Must not import Node-only modules
 
 export {
-  Aggregate,
-  AggregateMatch,
-  AggregateMatches,
   Aggregator,
-  CacheAdapter,
-  DuplicateError,
-  EmitterAdapter,
   Event,
   EventContext,
   EventData,
-  EventHandler,
   EventNamespace,
-  EventStore,
   EventType,
-  ExecuteF,
   Logger,
-  StoreAdapter,
-  Subscriptions,
-  ValidateF,
-} from ".";
+} from "./types";
 export { createEvent, createContext, isEventData, isEventContext, isEventType } from "./helpers";
