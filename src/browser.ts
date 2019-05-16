bc13a858b33c0329b746b0a3810ed619669cb408
James Waples <james@wapl.es>
2019-05-16 14:06:12 +0100

    Clean up browser exports
    It doesn't make sense to export these other functions. It also brings
    browser exports in line as a subset of Node exports

--- src/browser.ts
+++ src/browser.ts
@@ -1,4 +1,15 @@
 // Browser-only exports. Must not import Node-only modules
 
+<<<<<<< HEAD
 export { Event, EventContext, EventData, EventNamespace, EventType } from "./types";
+=======
+export {
+  Event,
+  EventContext,
+  EventData,
+  EventNamespace,
+  EventType,
+  Logger,
+} from "./types";
+>>>>>>> c37fed3... Clean up browser exports
 export { createEvent, createContext, isEventData } from "./helpers";
