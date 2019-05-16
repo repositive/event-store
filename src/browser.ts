e70741842e884668b499a6a2bc5e8025a32b9af1
James Waples <james@wapl.es>
2019-05-16 14:05:06 +0100

    Fmt

--- src/browser.ts
+++ src/browser.ts
@@ -1,12 +1,4 @@
 // Browser-only exports. Must not import Node-only modules
 
-export {
-  Aggregator,
-  Event,
-  EventContext,
-  EventData,
-  EventNamespace,
-  EventType,
-  Logger,
-} from "./types";
-export { createEvent, createContext, isEventData, isEventContext, isEventType } from "./helpers";
+export { Event, EventContext, EventData, EventNamespace, EventType } from "./types";
+export { createEvent, createContext, isEventData } from "./helpers";
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { createEvent, createContext, isEventType } from "./helpers";
-import {} from './types';
 export * from "./event-store";
 export * from "./adapters";
 export * from "./types";
