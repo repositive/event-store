350ec304f0393fdea8630d9ee878aad82b70a2fb
James Waples <james@wapl.es>
2019-05-16 14:09:37 +0100

    Fixup rebase

--- src/browser.ts
+++ src/browser.ts
@@ -1,15 +1,4 @@
 // Browser-only exports. Must not import Node-only modules
 
-<<<<<<< HEAD
 export { Event, EventContext, EventData, EventNamespace, EventType } from "./types";
-=======
-export {
-  Event,
-  EventContext,
-  EventData,
-  EventNamespace,
-  EventType,
-  Logger,
-} from "./types";
->>>>>>> c37fed3... Clean up browser exports
 export { createEvent, createContext, isEventData } from "./helpers";
